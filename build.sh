0b63af75f0e4fa2b66e2c5b2bda521f662210dff
Abdulla <abdullainomboyev@gmail.com>
2026-08-16 19:02:14 +0500

    seed_faculty

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_demo
+python manage.py seed_faculty || echo "seed_faculty o'tkazib yuborildi"
